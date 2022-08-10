821a54b9c52a547192704ca2af460a4f2fe1e1d9
Dale <47922140+kfaf@users.noreply.github.com>
2022-08-10 14:20:44 +0800

    Update Number of Provinces.cpp

--- Data Structure Application/Number of Provinces.cpp
+++ Data Structure Application/Number of Provinces.cpp
@@ -1,15 +1,17 @@
+
+// Problem: https://practice.geeksforgeeks.org/problems/number-of-provinces/1
 class Solution {
   public:
     int numProvinces(vector<vector<int>> adj, int V) {
     	
-    	//visited is for keeping track of already visited
-		//vertex to remove redunduncy and avoid permanent loop
+    	// visited is for keeping track of already visited
+	// vertex to remove redunduncy and avoid permanent loop
         vector<bool> visited(V,false);
         int provinces = 0;
         
-        //traverse each vertex in graph
+        // traverse each vertex in graph
         for(int i=0;i<V;i++){
-        	//if vertex is visited, skip
+            //if vertex is visited, skip
             if(visited[i])continue;
             provinces++;
             
